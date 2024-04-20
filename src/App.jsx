61589e614d782eb24920de3c0fc15f106050736f
Muchael123 <mainamichael2849@gmail.com>
2024-04-20 13:31:15 +0300

    Add (core): Add basic project structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/" element={<Homepage/>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
