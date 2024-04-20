a6fc12d2969e7b4a2d8314c463a0ade097bd96b9
Muchael123 <mainamichael2849@gmail.com>
2024-04-20 18:19:07 +0300

    Redefine(Core): redifine the navbar to have a scroll effect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,30 @@
-import React, { useEffect, useState } from 'react'
+import React, { lazy, useEffect, useState } from 'react'
 import { IoMenu } from "react-icons/io5";
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 
 function Header() {
-    const scrollYProgress = useScroll();  
-    useEffect(() => {
-        if (scrollYProgress > prevY) {
-            setHidden(true);
-        } else {
-            setHidden(false);
-        }
-        setPrevY(scrollYProgress);
-    }, [scrollYProgress]);
-    const [hidden, setHidden] = useState(false);
-    const [prevY, setPrevY] = useState(scrollYProgress);
-
-
-    const variants = {
-      visible: { opacity: 1, y: 0 },
-      hidden: { opacity: 0, y: -25 },
-    };
+  const { scrollY } = useScroll();
+  const [hidden, setHidden] = useState(false);
+   const variants = {
+    hidden: { y: "-100%" },
+     visible: { y: 0 },
+  };
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    const prev = scrollY.getPrevious();
+    if (latest > prev && latest > 150) {
+      setHidden(true);
+    } else {
+      setHidden(false);
+    }
+});
   return (
-      <div className="w-screen justify-center items-center flex flex-row ">
-          <motion.div className='top-0 sticky' style={{ scaleX: scrollYProgress }} />
-      <motion.div
-        variants={variants}
-        animate={hidden ? "hidden" : "visible"}
-        transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.6 }}
-        className="w-[90%] max-w-[780px] flex flex-row justify-between bg-blue-200 py-5 px-4 rounded-br-xl md:rounded-br-2xl md:rounded-bl-2xl rounded-bl-xl"
-      >
+    <motion.nav
+      animate={hidden ? "hidden" : "visible"}
+      variants={variants}
+      transition={{ duration: 0.35, ease: "easeInOut" }}
+      className="w-screen sticky top-0 justify-center items-center flex flex-row "
+    >
+      <div className="w-[90%]   max-w-[780px] flex flex-row justify-between bg-blue-200 py-5 px-4 rounded-br-xl md:rounded-br-2xl md:rounded-bl-2xl rounded-bl-xl">
         <div className="md:text-2xl text-xl font-semibold lg:text-3xl text-blue-500">
           Binary-Brigades CMS
         </div>
@@ -38,8 +34,8 @@ function Header() {
           </p>
           <IoMenu className="lg:text-3xl font-bold md:text-2xl text-xl hover:cursor-pointer" />
         </div>
-      </motion.div>
-    </div>
+      </div>
+    </motion.nav>
   );
 }
 
