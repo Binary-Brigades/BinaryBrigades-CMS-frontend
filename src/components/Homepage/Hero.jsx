97ba919dca2220fd23816082da864211d6c25d2a
Muchael123 <mainamichael2849@gmail.com>
2024-04-23 14:30:13 +0300

    ADD(Feature): add the Projectcard in homepage

--- src/components/Homepage/Hero.jsx
+++ src/components/Homepage/Hero.jsx
@@ -1,40 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProjectCard from './ProjectCard';
 
 export default function Hero() {
+    useEffect(() => {
+        getAll()
+    }, [])
+
+    const getAll = async () => {
+         const token = localStorage.getItem("authToken");
+        console.log('Mytoken',token)
+        try {
+            const response = await fetch("https://portfolio-cms-nine.vercel.app/api/v1/project/getall", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            const data = await response.json()
+            console.log(data)
+        } catch (error) {
+            console.error("An error occurred", error);
+        }
+    }
     return (
       <div className="w-screen justify-center items-center flex ">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 px-1 md:px-2 w-[90%] max-w-[780px] ">
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-            voluptates unde accusantium deleniti doloremque ad similique quae
-            cum excepturi amet in est nihil, explicabo, voluptas ullam tenetur?
-            Alias, dicta doloribus.
-            <hr className="h-[2px] bg-gray-400" />
-          </div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-            voluptates unde accusantium deleniti doloremque ad similique quae
-            cum excepturi amet in est nihil, explicabo, voluptas ullam tenetur?
-            Alias, dicta doloribus.
-          </div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-            voluptates unde accusantium deleniti doloremque ad similique quae
-            cum excepturi amet in est nihil, explicabo, voluptas ullam tenetur?
-            Alias, dicta doloribus.
-          </div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-            voluptates unde accusantium deleniti doloremque ad similique quae
-            cum excepturi amet in est nihil, explicabo, voluptas ullam tenetur?
-            Alias, dicta doloribus.
-          </div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-            voluptates unde accusantium deleniti doloremque ad similique quae
-            cum excepturi amet in est nihil, explicabo, voluptas ullam tenetur?
-            Alias, dicta doloribus.
-          </div>
+        <div className="grid grid-cols-1 md:grid-cols-2  gap-3 px-1 md:px-2 w-[90%] max-w-[780px] ">
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
         </div>
       </div>
     );
