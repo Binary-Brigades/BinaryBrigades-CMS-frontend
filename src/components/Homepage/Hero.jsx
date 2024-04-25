3fad3b876d80788a5e791a75b04bc955d2123b4b
Sebbie Evayo <sebbievilar2@gmail.com>
2024-04-25 15:12:09 +0300

    work on hero section

--- src/components/Homepage/Hero.jsx
+++ src/components/Homepage/Hero.jsx
@@ -1,41 +1,51 @@
-import React from 'react'
-
-export default function Hero() {
-    return (
-      <div className="w-screen justify-center items-center flex ">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 px-1 md:px-2 w-[90%] max-w-[780px] ">
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-            voluptates unde accusantium deleniti doloremque ad similique quae
-            cum excepturi amet in est nihil, explicabo, voluptas ullam tenetur?
-            Alias, dicta doloribus.
-            <hr className="h-[2px] bg-gray-400" />
-          </div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-            voluptates unde accusantium deleniti doloremque ad similique quae
-            cum excepturi amet in est nihil, explicabo, voluptas ullam tenetur?
-            Alias, dicta doloribus.
-          </div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-            voluptates unde accusantium deleniti doloremque ad similique quae
-            cum excepturi amet in est nihil, explicabo, voluptas ullam tenetur?
-            Alias, dicta doloribus.
-          </div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-            voluptates unde accusantium deleniti doloremque ad similique quae
-            cum excepturi amet in est nihil, explicabo, voluptas ullam tenetur?
-            Alias, dicta doloribus.
-          </div>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
-            voluptates unde accusantium deleniti doloremque ad similique quae
-            cum excepturi amet in est nihil, explicabo, voluptas ullam tenetur?
-            Alias, dicta doloribus.
-          </div>
-        </div>
-      </div>
-    );
+import React, {useState} from 'react'
+import image from '/tumeiget.png'
+function UserProfile({image, description}) {
+  return(
+    <div className='user-profile'>
+      <img src={image} alt="Profile Picture" />
+      <p>{description}</p>
+    </div>
+  )
+            
 }
+function Hero(){
+  const user = {
+    image,
+    description: 'software Engineer passionate about building user-friendly applications.'
+  }
+  return(
+    
+    <div className='App'>
+      <UserProfile image = {image} description = {user.description}/>
+      <p>IMAGE</p>
+    </div>
+  )
+}
+export default Hero;
+
+// export default function Hero() {
+  
+
+
+//       <div className="w-screen justify-center items-center flex ">
+
+//         {/* <img 
+//           src={image} alt="Project" 
+//           className="object-cover"
+//           height= "600"
+//           width="800"
+//           style={{
+//             aspectRatio: "800/400",
+//             objectFit: "cover"
+//           }}
+//           /> */}
+
+
+//         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 px-1 md:px-2 w-[90%] max-w-[780px] ">
+          
+          
+//         </div>
+//       </div>
+    
+// }
